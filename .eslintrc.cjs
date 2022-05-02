03213bc6979942a9d7998f8c2ca01562552240e8
Lucas Choi <lucas@deepnatural.ai>
2022-05-02 13:05:58 +0900

    Initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,31 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['svelte3', '@typescript-eslint'],
+  overrides: [
+    {
+      files: ['*.svelte'],
+      processor: 'svelte3/svelte3',
+    },
+  ],
+  globals: {
+    defineEmits: 'readonly',
+    defineExpose: 'readonly',
+    defineProps: 'readonly',
+    withDefaults: 'readonly',
+    $ref: 'readonly',
+    $computed: 'readonly',
+  },
+  rules: {
+    '@typescript-eslint/ban-types': 'off',
+  },
+  ignorePatterns: ['dist'],
+  settings: {
+    'svelte3/typescript': true,
+  },
+}
---
+++ src/App.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+</script>
+
+<main>
+  <div>Frontend Project</div>
+</main>
+
+<style lang="scss">
+:root {
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+    Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+}
+
+main {
+  text-align: center;
+  padding: 1em;
+  margin: 0 auto;
+}
+
+@media (min-width: 480px) {
+  p {
+    max-width: none;
+  }
+}
+</style>
