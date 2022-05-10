0e6342b30f76c9666c194ed00ca459a6c2616b49
Lucas Choi <lucas@deepnatural.ai>
2022-05-11 01:08:57 +0900

    feat: 날짜 네비게이터 기능 추가

---
+++ src/components/DateNavigator.svelte
@@ -0,0 +1,70 @@
+<script lang="ts">
+  import { currentDate, sideLength } from '@/store';
+  import { getNextDateString } from '@/utils';
+
+  const moveDate = (n: number) => {
+    currentDate.update((v) => getNextDateString(v, n));
+  };
+</script>
+
+<nav class:mobile={!$sideLength}>
+  <svg
+    class="arrow left"
+    width="64"
+    height="64"
+    viewBox="0 0 24 24"
+    xmlns="http://www.w3.org/2000/svg"
+    on:click={() => moveDate(-1)}
+  >
+    <path
+      d="M15.53 4.22a.75.75 0 0 1 0 1.06L8.81 12l6.72 6.72a.75.75 0 1 1-1.06 1.06l-7.25-7.25a.75.75 0 0 1 0-1.06l7.25-7.25a.75.75 0 0 1 1.06 0Z"
+    />
+  </svg>
+  <svg
+    class="arrow right"
+    width="64"
+    height="64"
+    viewBox="0 0 24 24"
+    xmlns="http://www.w3.org/2000/svg"
+    on:click={() => moveDate(1)}
+  >
+    <path
+      d="M8.47 4.22a.75.75 0 0 0 0 1.06L15.19 12l-6.72 6.72a.75.75 0 1 0 1.06 1.06l7.25-7.25a.75.75 0 0 0 0-1.06L9.53 4.22a.75.75 0 0 0-1.06 0Z"
+    />
+  </svg>
+</nav>
+
+<style lang="scss">
+  nav {
+    .arrow {
+      position: fixed;
+      top: 50%;
+      margin-top: -32px;
+      fill: #888;
+      cursor: pointer;
+
+      &.left {
+        left: 0;
+      }
+      &.right {
+        right: 0;
+      }
+    }
+
+    &.mobile .arrow {
+      position: absolute;
+      top: 0;
+      width: 20px;
+      height: 20px;
+      margin-top: 20px;
+      fill: #666;
+
+      &.left {
+        margin-left: 20px;
+      }
+      &.right {
+        margin-right: 20px;
+      }
+    }
+  }
+</style>
